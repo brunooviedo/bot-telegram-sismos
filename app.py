fa77871de66f93783f2a4e69dacfb7f523f4af96
bruno <33145112+brunooviedo@users.noreply.github.com>
2024-03-26 04:18:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 def bot_send_text(bot_message):
     
-    bot_token = '5231406261:AAE1lr7A9feeiv9Ejt3awEyigwzpxtyoqRo'
-    bot_chatID = '-796627951'
+    bot_token = '******'
+    bot_chatID = '*******'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
